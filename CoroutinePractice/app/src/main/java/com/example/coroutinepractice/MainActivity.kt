6f3063328d54ef3435f0b5dcb1298ff96c383bee
Pornpat Santibuppakul <dottypurkt@gmail.com>
2020-12-23 18:38:44 +0700

    CoroutinePractice - Implement UI's action (Start/Cancel each Job, Start/Cancel all Job)

--- CoroutinePractice/app/src/main/java/com/example/coroutinepractice/MainActivity.kt
+++ CoroutinePractice/app/src/main/java/com/example/coroutinepractice/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.coroutinepractice
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.coroutinepractice.databinding.ActivityMainBinding
+import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,47 +24,103 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            viewModel.runAllProcess()
+        binding.run {
+            startAllButton.setOnClickListener {
+                viewModel.runAllJob()
+            }
+
+            cancelAllButton.setOnClickListener {
+                viewModel.cancelAllJob()
+            }
+
+            startButtonJob1.setOnClickListener {
+                val job = viewModel.job1
+
+                if (job == null) {
+                    viewModel.startJob1()
+                } else if (!job.isCompleted) {
+                    viewModel.cancelJob1()
+                }
+            }
+
+            startButtonJob2.setOnClickListener {
+                val job = viewModel.job2
+
+                if (job == null) {
+                    viewModel.startJob2()
+                } else if (!job.isCompleted) {
+                    viewModel.cancelJob2()
+                }
+            }
+
+            startButtonJob3.setOnClickListener {
+                val job = viewModel.job3
+
+                if (job == null) {
+                    viewModel.startJob3()
+                } else if (!job.isCompleted) {
+                    viewModel.cancelJob3()
+                }
+            }
         }
 
         // Observe
         viewModel.run {
-            job1LiveData.observe(this@MainActivity) {
-                binding.job1TextView.run {
-                    text = "$it %"
+            val colorRed = Color.RED
+            val colorBlue = getColor(R.color.colorBlue)
+            val colorGreenYellow = getColor(R.color.colorGreenYellow)
 
-                    if (it == 100) {
-                        this.setTextColor(Color.GREEN)
-                    }
+            job1LiveData.observe(this@MainActivity) {
+                binding.run {
+                    updateJobUi(
+                        it, viewModel.job1, "Start 1", colorRed,
+                        startButtonJob1, progressBarJob1, job1TextView
+                    )
                 }
             }
 
             job2LiveData.observe(this@MainActivity) {
-                binding.job2TextView.run {
-                    text = "$it %"
-
-                    if (it == 100) {
-                        this.setTextColor(Color.GREEN)
-                    }
+                binding.run {
+                    updateJobUi(
+                        it, viewModel.job2, "Start 2", colorBlue,
+                        startButtonJob2, progressBarJob2, job2TextView
+                    )
                 }
             }
 
             job3LiveData.observe(this@MainActivity) {
-                binding.job3TextView.run {
-                    text = "$it %"
-
-                    if (it == 100) {
-                        this.setTextColor(Color.GREEN)
-                    }
+                binding.run {
+                    updateJobUi(
+                        it, viewModel.job3, "Start 3", colorGreenYellow,
+                        startButtonJob3, progressBarJob3, job3TextView
+                    )
                 }
             }
 
             statusLiveData.observe(this@MainActivity) { isDone ->
                 if (isDone) {
-                    binding.root.setBackgroundColor(Color.GREEN)
+                    Toast.makeText(this@MainActivity, "All job is done!", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+    }
+
+    private fun updateJobUi(percent: Int, job: Job?, mainText: String, mainColor: Int,
+                            button: Button, progressBar: ProgressBar, percentTextView: TextView) {
+        val colorDarkGray = Color.GRAY
+
+        percentTextView.text = "$percent %"
+        progressBar.progress = percent
+
+        if (percent == 100 || percent == 0) {
+            button.setBackgroundColor(mainColor)
+            button.text = mainText
+            percentTextView.setTextColor(
+                if (percent == 100) mainColor else colorDarkGray
+            )
+        } else if (job?.isActive == true) {
+            button.setBackgroundColor(colorDarkGray)
+            button.text = "Cancel"
         }
     }
 }
--- CoroutinePractice/app/src/main/java/com/example/coroutinepractice/MainViewModel.kt
+++ CoroutinePractice/app/src/main/java/com/example/coroutinepractice/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.coroutinepractice
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,6 +7,10 @@ import kotlinx.coroutines.*
 import kotlin.random.Random
 
 class MainViewModel: ViewModel() {
+    // Job
+    var job1: Job? = null
+    var job2: Job? = null
+    var job3: Job? = null
 
     // LiveData
     val job1LiveData = MutableLiveData<Int>().apply { value = 0 }
@@ -15,29 +18,103 @@ class MainViewModel: ViewModel() {
     val job3LiveData = MutableLiveData<Int>().apply { value = 0 }
     val statusLiveData = MutableLiveData<Boolean>().apply { value = false }
 
-    fun runAllProcess() {
+    fun runAllJob() {
+        // reset old status and each job value
+        reset()
+
         viewModelScope.launch(Dispatchers.Main) {
-            val result1 = async { increase(200, job1LiveData) }
-            val result2 = async { increase(100, job2LiveData) }
-            val result3 = async { increase(50, job3LiveData) }
-            val deferredList = listOf(result1, result2, result3)
-            if (!deferredList.awaitAll().contains(false)) {
+            startJob1()
+            startJob2()
+            startJob3()
+
+            // wait each job to be finished.
+            val jobs = listOfNotNull(job1, job2, job3)
+            jobs.joinAll()
+
+            val hasCompletedAllJob = jobs.find { it.isCancelled } == null
+            if (hasCompletedAllJob) {
                 statusLiveData.postValue(true)
             }
+
+//            val deferredList = listOf(result1, result2, result3)
+//            if (!deferredList.awaitAll().contains(false)) {
+//                statusLiveData.postValue(true)
+//            }
         }
     }
 
-    private suspend fun increase(speed: Long? = null, liveData: MutableLiveData<Int>): Boolean {
-        return withContext(Dispatchers.IO) {
+    fun startJob1() {
+        job1 = viewModelScope.launch(Dispatchers.IO) {
             var percent = 0
-            while (percent < 100) {
-                Thread.sleep(speed ?: random())
-                liveData.postValue(++percent)
+            while (percent < 100 && isActive) {
+                Thread.sleep(200 ?: random())
+                job1LiveData.postValue(++percent)
             }
 
-            return@withContext true
+            job1 = null
         }
+    }
+
+    fun startJob2() {
+        job2 = viewModelScope.launch(Dispatchers.IO) {
+            var percent = 0
+            while (percent < 100 && isActive) {
+                Thread.sleep(100 ?: random())
+                job2LiveData.postValue(++percent)
+            }
+
+            job2 = null
+        }
+    }
+
+    fun startJob3() {
+        job3 = viewModelScope.launch(Dispatchers.IO) {
+            var percent = 0
+            while (percent < 100 && isActive) {
+                Thread.sleep(50 ?: random())
+                job3LiveData.postValue(++percent)
+            }
+
+            job3 = null
+        }
+    }
+
+    fun cancelJob1() {
+        viewModelScope.launch {
+            job1?.cancelAndJoin()
+            job1LiveData.postValue(0)
+            job1 = null
+        }
+    }
+
+    fun cancelJob2() {
+        viewModelScope.launch {
+            job2?.cancelAndJoin()
+            job2LiveData.postValue(0)
+            job2 = null
+        }
+    }
+
+    fun cancelJob3() {
+        viewModelScope.launch {
+            job3?.cancelAndJoin()
+            job3LiveData.postValue(0)
+            job3 = null
+        }
+    }
+
+    fun cancelAllJob() {
+        cancelJob1()
+        cancelJob2()
+        cancelJob3()
     }
 
     private fun random() = Random.nextLong(20, 1000)
+
+    private fun reset() {
+        job1LiveData.postValue(0)
+        job2LiveData.postValue(0)
+        job3LiveData.postValue(0)
+        statusLiveData.postValue(false)
+    }
 }
